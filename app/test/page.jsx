2a601aae1c662a5e70370331c8ad5a3967874b40
WTechy <wtechy@gmail.com>
2024-07-29 13:01:28 +0530

    styles

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,14 +1,14 @@
 import InnerBanner from "./../../common/InnerBanner";
 export default function Test() {
-    return (
-      <> <InnerBanner
-      title={"WEED WEED WEED WEED"}
-      info={
-        "WTechy helps businesses thrive online with expert Digital Marketing Strategies and Web Design Solutions. We turn possibilities into reality for your Brand."
-      }
-    />
- </>
-     
-    );
-  }
-  
+  return (
+    <>
+      
+      <InnerBanner
+        title={"WEED WEED WEED WEED"}
+        info={
+          "WTechy helps businesses thrive online with expert Digital Marketing Strategies and Web Design Solutions. We turn possibilities into reality for your Brand."
+        }
+      />
+    </>
+  );
+}
