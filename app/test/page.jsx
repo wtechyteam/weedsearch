af8069ad3d72af4b4acf99e54b5724a36ebecd84
WTechy <wtechy@gmail.com>
2024-07-29 12:56:13 +0530

    frontend styles

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,6 +1,14 @@
+import InnerBanner from "./../../common/InnerBanner";
 export default function Test() {
     return (
-    <div className="flex flex-col items-center justify-center h-screen"> TEST WEED  WEED WEED!!!</div>
+      <> <InnerBanner
+      title={"WEED WEED WEED WEED"}
+      info={
+        "WTechy helps businesses thrive online with expert Digital Marketing Strategies and Web Design Solutions. We turn possibilities into reality for your Brand."
+      }
+    />
+    <div className="flex flex-col items-center justify-center h-screen"> TEST WEED  WEED WEED!!!</div></>
+     
     );
   }
   
